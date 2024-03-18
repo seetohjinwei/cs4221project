223df0f3eb5f8b7a66705e95902af460022b7d21
cheweejia <eejiachew@gmail.com>
2024-03-18 18:48:20 +0800

    Fix filtering of tokens to remove extra commas

--- schema_scripts/simple.sql
+++ schema_scripts/simple.sql
@@ -3,4 +3,15 @@ CREATE TABLE hello (
     b INTEGER,
     CONSTRAINT b_pk PRIMARY KEY (b),
     c INTEGER CHECK (c < 0)
-)
+);
+
+CREATE TABLE bye (
+    CHECK (a == 0),
+    CONSTRAINT ac CHECK (a != 1),
+    a INTEGER,
+    CHECK (a > 0),
+    c INTEGER, 
+    b INTEGER,
+    CONSTRAINT cc CHECK (c < 0),
+    CHECK (b < 0)
+);

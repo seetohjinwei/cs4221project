9c07d27581b0eb3999d6769cb76a9cd51cfabc02
tyx021 <yxting1109@gmail.com>
2024-03-30 21:23:24 +0800

    fix test3 transformed

--- benchmark/bench_data/test3/schema/transformed.sql
+++ benchmark/bench_data/test3/schema/transformed.sql
@@ -7,217 +7,217 @@ CREATE TABLE T1 ( A INT , B VARCHAR(100) , C VARCHAR(100) , D VARCHAR(100) , E T
 
 
                     --- Trigger ---
-                    CREATE OR REPLACE FUNCTION T1_1516()
+                    CREATE OR REPLACE FUNCTION T1_3261()
                     RETURNS TRIGGER AS $$
                     BEGIN
-                        IF NOT (NNEW.EW.A > 0 NNEW.EW.ANNNEW.EW.D LNEW.ENGTH(NNEW.EW.B) <= 100) THEN
-                            RAISE EXCEPTION 'T1_1516 constraint violated';
+                        IF NOT (NEW.A > 0 AND LENGTH(NEW.B) <= 100) THEN
+                            RAISE EXCEPTION 'T1_3261 constraint violated';
                         END IF;
                         RETURN NEW;
                     END;
                     $$ LANGUAGE plpgsql;
 
-                    CREATE TRIGGER T1_1516_trigger
+                    CREATE TRIGGER T1_3261_trigger
                     BEFORE INSERT OR UPDATE ON T1
                     FOR EACH ROW
-                    EXECUTE FUNCTION T1_1516();
+                    EXECUTE FUNCTION T1_3261();
                     ---------------
                 
 
                     --- Trigger ---
-                    CREATE OR REPLACE FUNCTION T2_28909()
+                    CREATE OR REPLACE FUNCTION T2_45376()
                     RETURNS TRIGGER AS $$
                     BEGIN
                         IF NOT (NEW.H >= 0) THEN
-                            RAISE EXCEPTION 'T2_28909 constraint violated';
+                            RAISE EXCEPTION 'T2_45376 constraint violated';
                         END IF;
                         RETURN NEW;
                     END;
                     $$ LANGUAGE plpgsql;
 
-                    CREATE TRIGGER T2_28909_trigger
+                    CREATE TRIGGER T2_45376_trigger
                     BEFORE INSERT OR UPDATE ON T2
                     FOR EACH ROW
-                    EXECUTE FUNCTION T2_28909();
+                    EXECUTE FUNCTION T2_45376();
                     ---------------
                 
 
                     --- Trigger ---
-                    CREATE OR REPLACE FUNCTION T3_70147()
+                    CREATE OR REPLACE FUNCTION T3_37303()
                     RETURNS TRIGGER AS $$
                     BEGIN
                         IF NOT (NEW.K > 0 AND NEW.M >= 0) THEN
-                            RAISE EXCEPTION 'T3_70147 constraint violated';
+                            RAISE EXCEPTION 'T3_37303 constraint violated';
                         END IF;
                         RETURN NEW;
                     END;
                     $$ LANGUAGE plpgsql;
 
-                    CREATE TRIGGER T3_70147_trigger
+                    CREATE TRIGGER T3_37303_trigger
                     BEFORE INSERT OR UPDATE ON T3
                     FOR EACH ROW
-                    EXECUTE FUNCTION T3_70147();
+                    EXECUTE FUNCTION T3_37303();
                     ---------------
                 
 
                     --- Trigger ---
-                    CREATE OR REPLACE FUNCTION T4_98591()
+                    CREATE OR REPLACE FUNCTION T4_43467()
                     RETURNS TRIGGER AS $$
                     BEGIN
-                        IF NOT (NEW.O > 0 ANEW.ND NEW.Q > 0) THEN
-                            RAISE EXCEPTION 'T4_98591 constraint violated';
+                        IF NOT (NEW.O > 0 AND NEW.Q > 0) THEN
+                            RAISE EXCEPTION 'T4_43467 constraint violated';
                         END IF;
                         RETURN NEW;
                     END;
                     $$ LANGUAGE plpgsql;
 
-                    CREATE TRIGGER T4_98591_trigger
+                    CREATE TRIGGER T4_43467_trigger
                     BEFORE INSERT OR UPDATE ON T4
                     FOR EACH ROW
-                    EXECUTE FUNCTION T4_98591();
+                    EXECUTE FUNCTION T4_43467();
                     ---------------
                 
 
                     --- Trigger ---
-                    CREATE OR REPLACE FUNCTION T5_70927()
+                    CREATE OR REPLACE FUNCTION T5_36718()
                     RETURNS TRIGGER AS $$
                     BEGIN
-                        IF NOT (NENEW.W.R > 0) THEN
-                            RAISE EXCEPTION 'T5_70927 constraint violated';
+                        IF NOT (NEW.R > 0) THEN
+                            RAISE EXCEPTION 'T5_36718 constraint violated';
                         END IF;
                         RETURN NEW;
                     END;
                     $$ LANGUAGE plpgsql;
 
-                    CREATE TRIGGER T5_70927_trigger
+                    CREATE TRIGGER T5_36718_trigger
                     BEFORE INSERT OR UPDATE ON T5
                     FOR EACH ROW
-                    EXECUTE FUNCTION T5_70927();
+                    EXECUTE FUNCTION T5_36718();
                     ---------------
                 
 
                     --- Trigger ---
-                    CREATE OR REPLACE FUNCTION T5_62483()
+                    CREATE OR REPLACE FUNCTION T5_16729()
                     RETURNS TRIGGER AS $$
                     BEGIN
-                        IF NOT (NENEW.W.S BENENEW.W.TNEW.WEEN 1 AND 100) THEN
-                            RAISE EXCEPTION 'T5_62483 constraint violated';
+                        IF NOT (NEW.S BETWEEN 1 AND 100) THEN
+                            RAISE EXCEPTION 'T5_16729 constraint violated';
                         END IF;
                         RETURN NEW;
                     END;
                     $$ LANGUAGE plpgsql;
 
-                    CREATE TRIGGER T5_62483_trigger
+                    CREATE TRIGGER T5_16729_trigger
                     BEFORE INSERT OR UPDATE ON T5
                     FOR EACH ROW
-                    EXECUTE FUNCTION T5_62483();
+                    EXECUTE FUNCTION T5_16729();
                     ---------------
                 
 
                     --- Trigger ---
-                    CREATE OR REPLACE FUNCTION T5_92060()
+                    CREATE OR REPLACE FUNCTION T5_42361()
                     RETURNS TRIGGER AS $$
                     BEGIN
-                        IF NOT (LENGNENEW.W.TH(NENEW.W.T) <= 100) THEN
-                            RAISE EXCEPTION 'T5_92060 constraint violated';
+                        IF NOT (LENGTH(NEW.T) <= 100) THEN
+                            RAISE EXCEPTION 'T5_42361 constraint violated';
                         END IF;
                         RETURN NEW;
                     END;
                     $$ LANGUAGE plpgsql;
 
-                    CREATE TRIGGER T5_92060_trigger
+                    CREATE TRIGGER T5_42361_trigger
                     BEFORE INSERT OR UPDATE ON T5
                     FOR EACH ROW
-                    EXECUTE FUNCTION T5_92060();
+                    EXECUTE FUNCTION T5_42361();
                     ---------------
                 
 
                     --- Trigger ---
-                    CREATE OR REPLACE FUNCTION T5_46393()
+                    CREATE OR REPLACE FUNCTION T5_36520()
                     RETURNS TRIGGER AS $$
                     BEGIN
-                        IF NOT (NENEW.W.U >= 0 AND NENEW.W.U <= 10000) THEN
-                            RAISE EXCEPTION 'T5_46393 constraint violated';
+                        IF NOT (NEW.U >= 0 AND NEW.U <= 10000) THEN
+                            RAISE EXCEPTION 'T5_36520 constraint violated';
                         END IF;
                         RETURN NEW;
                     END;
                     $$ LANGUAGE plpgsql;
 
-                    CREATE TRIGGER T5_46393_trigger
+                    CREATE TRIGGER T5_36520_trigger
                     BEFORE INSERT OR UPDATE ON T5
                     FOR EACH ROW
-                    EXECUTE FUNCTION T5_46393();
+                    EXECUTE FUNCTION T5_36520();
                     ---------------
                 
 
                     --- Trigger ---
-                    CREATE OR REPLACE FUNCTION T5_9150()
+                    CREATE OR REPLACE FUNCTION T5_90674()
                     RETURNS TRIGGER AS $$
                     BEGIN
-                        IF NOT (NENEW.W.V INENEW.W.S NONENEW.W.T NNENEW.W.ULL) THEN
-                            RAISE EXCEPTION 'T5_9150 constraint violated';
+                        IF NOT (NEW.V IS NOT NULL) THEN
+                            RAISE EXCEPTION 'T5_90674 constraint violated';
                         END IF;
                         RETURN NEW;
                     END;
                     $$ LANGUAGE plpgsql;
 
-                    CREATE TRIGGER T5_9150_trigger
+                    CREATE TRIGGER T5_90674_trigger
                     BEFORE INSERT OR UPDATE ON T5
                     FOR EACH ROW
-                    EXECUTE FUNCTION T5_9150();
+                    EXECUTE FUNCTION T5_90674();
                     ---------------
                 
 
                     --- Trigger ---
-                    CREATE OR REPLACE FUNCTION T5_57443()
+                    CREATE OR REPLACE FUNCTION T5_20716()
                     RETURNS TRIGGER AS $$
                     BEGIN
-                        IF NOT (NEW.W INENEW.W.S NONENEW.W.T FALNENEW.W.SE) THEN
-                            RAISE EXCEPTION 'T5_57443 constraint violated';
+                        IF NOT (NEW.W IS NOT FALSE) THEN
+                            RAISE EXCEPTION 'T5_20716 constraint violated';
                         END IF;
                         RETURN NEW;
                     END;
                     $$ LANGUAGE plpgsql;
 
-                    CREATE TRIGGER T5_57443_trigger
+                    CREATE TRIGGER T5_20716_trigger
                     BEFORE INSERT OR UPDATE ON T5
                     FOR EACH ROW
-                    EXECUTE FUNCTION T5_57443();
+                    EXECUTE FUNCTION T5_20716();
                     ---------------
                 
 
                     --- Trigger ---
-                    CREATE OR REPLACE FUNCTION T5_53369()
+                    CREATE OR REPLACE FUNCTION T5_47978()
                     RETURNS TRIGGER AS $$
                     BEGIN
-                        IF NOT (NEW.X BENENEW.W.TNEW.WEEN 0.0 AND 1.0) THEN
-                            RAISE EXCEPTION 'T5_53369 constraint violated';
+                        IF NOT (NEW.X BETWEEN 0.0 AND 1.0) THEN
+                            RAISE EXCEPTION 'T5_47978 constraint violated';
                         END IF;
                         RETURN NEW;
                     END;
                     $$ LANGUAGE plpgsql;
 
-                    CREATE TRIGGER T5_53369_trigger
+                    CREATE TRIGGER T5_47978_trigger
                     BEFORE INSERT OR UPDATE ON T5
                     FOR EACH ROW
-                    EXECUTE FUNCTION T5_53369();
+                    EXECUTE FUNCTION T5_47978();
                     ---------------
                 
 
                     --- Trigger ---
-                    CREATE OR REPLACE FUNCTION T5_12789()
+                    CREATE OR REPLACE FUNCTION T5_30995()
                     RETURNS TRIGGER AS $$
                     BEGIN
                         IF NOT (NEW.Y IN ('A', 'B', 'C')) THEN
-                            RAISE EXCEPTION 'T5_12789 constraint violated';
+                            RAISE EXCEPTION 'T5_30995 constraint violated';
                         END IF;
                         RETURN NEW;
                     END;
                     $$ LANGUAGE plpgsql;
 
-                    CREATE TRIGGER T5_12789_trigger
+                    CREATE TRIGGER T5_30995_trigger
                     BEFORE INSERT OR UPDATE ON T5
                     FOR EACH ROW
-                    EXECUTE FUNCTION T5_12789();
+                    EXECUTE FUNCTION T5_30995();
                     ---------------
                 
